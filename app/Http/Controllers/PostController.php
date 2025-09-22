37d683dbc2da6fca47e6d14c9cc25e430c149b33
fewioaghwrao <nrk13959@gmail.com>
2025-09-23 08:43:43 +0900

    first commit: add Laravel project

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index() {
+        // postsテーブルからすべてのデータを取得し、変数$productsに代入する
+        $posts = DB::table('posts')->get();
+
+        // 変数$postsをposts/index.blade.phpファイルに渡す
+        return view('posts.index', compact('posts'));
+    }    
+
+    public function show($id) {
+        // URL'/posts/{id}'の'{id}'部分と主キー（idカラム）の値が一致するデータをpostsテーブルから取得し、変数$postに代入する
+        $post = Post::findOrFail($id);
+
+        // 変数$postをposts/show.blade.phpファイルに渡す
+        return view('posts.show', compact('post'));
+    }
+
+    public function create() {
+        return view('posts.create');
+    } 
+        
+    public function store(Request $request) {
+        // バリデーションを設定する
+        $request->validate([
+            'title' => 'required|max:20',
+            'content' => 'required|max:200'
+        ]);
+
+        // フォームの入力内容をもとに、テーブルにデータを追加する
+        $posts = new Post();
+        $posts-> title = $request->input('title');
+        $posts-> content = $request->input('content');
+        $posts->save();
+
+        // リダイレクトさせる
+        return redirect("/posts");
+    }    
+}
